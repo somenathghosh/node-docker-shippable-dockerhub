8da7b785be5de4de668cab96c622702b574cf2cf
EC2 Default User <ec2-user@ip-172-31-39-28.us-west-2.compute.internal>
2016-04-14 12:04:30 +0000

    02

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-'use strict';
 
-const express = require('express');
+var express = require('express');
 
 
 // Constants
-const PORT = 12021;
+var PORT = 12021;
 
 // App
-const app = express();
+var app = express();
 app.get('/', function (req, res) {
    res.send('Hey Buddy...!!!');
 });
